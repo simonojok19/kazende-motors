96cfd634d9d0d154ec6b71cad8faf068e230f916
simonojok19 <simonojok19@gmail.com>
2020-03-23 15:09:42 +0300

    added the toolbar and icon module to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { from } from 'rxjs';
@@ -13,6 +16,8 @@ import { from } from 'rxjs';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MatIconModule,
+    MatIconModule,
     AppRoutingModule
   ],
   providers: [],
